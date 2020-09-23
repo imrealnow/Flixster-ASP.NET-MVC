aaad82629cf285161521eff49df08b5ed719af47
Liam Green <imrealnow95@gmail.com>
2020-09-24 03:20:54 +1200

    Finished making customer creation functionality

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -50,5 +50,14 @@ namespace Flixster.Controllers
 
             return View(viewModel);
         }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
     }
 }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,6 +9,7 @@ namespace Flixster.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required, StringLength(255)]
         public string Name { get; set; }
 
@@ -16,7 +17,10 @@ namespace Flixster.Models
         public DateTime? Birthdate { get; set; }
 
         public bool isSubscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
     }
 }
