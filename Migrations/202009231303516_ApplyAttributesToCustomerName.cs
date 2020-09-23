3187cbd62f96e2d16fb8df0ff909c5848c84a296
Liam Green <imrealnow95@gmail.com>
2020-09-24 01:13:16 +1200

    *Added MembershipType Model *Created SQL Express database *Updated CustomerController to query the database

---
+++ Migrations/202009231303516_ApplyAttributesToCustomerName.cs
@@ -0,0 +1,18 @@
+namespace Flixster.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplyAttributesToCustomerName : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+        }
+    }
+}
