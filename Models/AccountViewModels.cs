b916d7905dee9f2d06917c4b619e4dda0b639ebd
Liam Green <imrealnow95@gmail.com>
2020-09-29 23:41:02 +1300

    Add driving license field to identity model

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -70,6 +70,11 @@ namespace Flixster.Models
         public string Email { get; set; }
 
         [Required]
+        [Display(Name = "Driver's License ID")]
+        [StringLength(255)]
+        public string DrivingLicense { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
